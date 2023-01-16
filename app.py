1a9e938908a8d4ea0e5711d9fae1e3a2e781b673
leona <leonardofcoceschin@gmail.com>
2023-01-16 02:02:18 -0300

    editado regra upload, adicionado footer

--- app.py
+++ app.py
@@ -1,42 +1,40 @@
 import os
-from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory, send_file
+from flask import Flask, after_this_request, render_template, request, flash, redirect, send_from_directory
 from werkzeug.utils import secure_filename
-from exiftool import ExifTool
 import subprocess
 
 UPLOAD_FOLDER = './upload'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        if file and allowed_file(file.filename):
+        if file:
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            exiftool_command = ["exiftool", "-all=", app.config['UPLOAD_FOLDER']+"/"+filename, app.config['UPLOAD_FOLDER']+"/"+filename]
+            file_for_save = app.config['UPLOAD_FOLDER']+"/"+filename
+            exiftool_command = ["exiftool", "-all=", "-overwrite_original", file_for_save, file_for_save]
             subprocess.run(exiftool_command)
-            
-            return download_page(filename)
+            return download_file(filename)
     return render_template('index.html')
 
 @app.route('/download/<name>')
-def download_page(filename):
+def download_file(filename):
+    @after_this_request
+    def remove_file(response):
+        try:
+            os.remove(app.config['UPLOAD_FOLDER']+"/"+filename)
+        except Exception as error:
+            app.logger.error("Error removing or closing downloaded file handle", error)
+        return response
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
